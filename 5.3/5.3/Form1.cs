d5c87c6606c67f21ee1fa654c9c0340e24f7243b
Adam Morelius <adamor0202@skynet.halmstad.se>
2019-09-30 13:35:52 +0200

    A

---
+++ 5.3/5.3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSvar_Click(object sender, EventArgs e)
+        {
+            double varansPris = double.Parse(textBox2.Text);
+            double minaPengar = double.Parse(textBox1.Text);
+            bool JagHarRåd = varansPris <= minaPengar;
+            lblSvar.Text = "Jag har råd: " + JagHarRåd.ToString();
+        }
+    }
+}
