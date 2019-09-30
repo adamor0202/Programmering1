c8e320202fe769b0f7c41b342eedd9ccdc779278
Adam Morelius <adamor0202@skynet.halmstad.se>
2019-09-30 13:27:58 +0200

    Hej

---
+++ 30sep/30sep/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30sep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Knapp_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = a--;
+            int c = ++b + a;
+
+            label2.Text = c.ToString();
+        }
+    }
+}
