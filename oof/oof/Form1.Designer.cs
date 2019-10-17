e29500f622e4fb3ac345320ff6daff97ebefb0da
Adam Morelius <adamor0202@skynet.halmstad.se>
2019-10-17 12:57:58 +0200

    7.7

---
+++ oof/oof/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace oof
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonsvar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labeltal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tal 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tal 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(316, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(316, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // buttonsvar
+            // 
+            this.buttonsvar.Location = new System.Drawing.Point(327, 215);
+            this.buttonsvar.Name = "buttonsvar";
+            this.buttonsvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonsvar.TabIndex = 4;
+            this.buttonsvar.Text = "KÖR";
+            this.buttonsvar.UseVisualStyleBackColor = true;
+            this.buttonsvar.Click += new System.EventHandler(this.Buttonsvar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Det minsta talet är";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // labeltal
+            // 
+            this.labeltal.AutoSize = true;
+            this.labeltal.Location = new System.Drawing.Point(378, 175);
+            this.labeltal.Name = "labeltal";
+            this.labeltal.Size = new System.Drawing.Size(0, 13);
+            this.labeltal.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeltal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonsvar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonsvar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labeltal;
+    }
+}
+
---
+++ oof/oof/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oof
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Buttonsvar_Click(object sender, EventArgs e)
+        {
+            double tal1 = double.Parse(textBox1.Text);
+            double tal2 = double.Parse(textBox2.Text);
+
+            if (tal1 < tal2)
+            {
+                labeltal.Text = tal1.ToString();
+            }
+
+
+            else if (tal2 < tal1)
+            {
+                labeltal.Text = tal2.ToString();
+            }
+
+            else if (tal2 == tal1)
+            {
+                labeltal.Text = "ingen, de är lika stora";
+            }
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
